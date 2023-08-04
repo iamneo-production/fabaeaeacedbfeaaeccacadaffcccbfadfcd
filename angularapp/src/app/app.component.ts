6f3a9922b3e694c0b058a8520cb4f2a5f021ae86
Iamneo <techadmin@iamneo.ai>
2023-08-04 12:22:59 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularapp';
+  usd =  { "id":"USD", "value": 1.126735};
+  gbp=  { "id":"GBP", "value":0.876893};
+  inr=  { "id":"INR", "value":79.677056};
+
+  amount = 0;
+  fromCurrency='';
+  toCurrency=''; 
+  uid ='';
+  uval=0;
+  gid ='';
+  gval=0;
+  iid ='';
+  ival=0;
+
+  getValues(val:any){
+    this.amount= val.amount;
+    this.fromCurrency=val.fromCurrency;
+    this.toCurrency=val.toCurrency;
+    this.uid=this.usd.id;
+    this.uval=this.usd.value;
+    this.gid=this.gbp.id;
+    this.gval=this.gbp.value;
+    this.iid=this.inr.id;
+    this.ival=this.inr.value;
+}
 }
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { CurrconvPipe } from './currencypipe.pipe'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrconvPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
